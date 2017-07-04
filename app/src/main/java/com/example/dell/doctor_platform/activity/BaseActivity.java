50687cdedc320d906c9827acf0f886ac8c4e7329
kentanvictor <2030457926@qq.com>
2017-07-04 17:51:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/doctor_platform/activity/BaseActivity.java
@@ -0,0 +1,27 @@
+package com.example.dell.doctor_platform.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 2017/7/4.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+   /* private ForceOfflineReceiver receiver;
+
+    class ForceOfflineReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline.Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/dell/doctor_platform/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dell.doctor_platform.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.dell.doctor_platform.R;
+
+public class MainActivity extends BaseActivity{
+    private Button lbsActivity ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lbsActivity = (Button) findViewById(R.id.LBS_Activity);
+        lbsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LBSActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
