16ab72b3a4a5e442f29ebca8e59a5b36ea81f3ac
kentanvictor <2030457926@qq.com>
2017-07-11 16:20:56 +0800

    fail everything

--- app/src/main/java/com/example/dell/doctor_platform/activity/MainActivity.java
+++ app/src/main/java/com/example/dell/doctor_platform/activity/MainActivity.java
@@ -3,20 +3,13 @@ package com.example.dell.doctor_platform.activity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.FrameLayout;
 
 import com.example.dell.doctor_platform.R;
 import com.example.dell.doctor_platform.fragment.Homefragment;
 import com.example.dell.doctor_platform.fragment.LBSFragment;
 import com.example.dell.doctor_platform.fragment.Searchfragment;
 import com.example.dell.doctor_platform.fragment.SetFragment;
-
-import java.util.ArrayList;
 
 public class MainActivity extends SingleFragmentActivity implements
         BottomNavigationView.OnNavigationItemSelectedListener {
@@ -62,51 +55,5 @@ public class MainActivity extends SingleFragmentActivity implements
                 break;
         }
         return true;
-    }
-
-
-    public static class HomepagerAdapter extends PagerAdapter {
-        private ArrayList<FrameLayout> viewList;
-
-        public HomepagerAdapter(ArrayList<FrameLayout> viewList) {
-            this.viewList = viewList;
-        }
-
-        @Override
-        public int getCount() {
-            return Integer.MAX_VALUE;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            position %= viewList.size();
-            if (position < 0) {
-                position = viewList.size() + position;
-            }
-            FrameLayout view = viewList.get(position);
-            //如果View已经在之前添加到了一个父组件，则必须先remove，否则会抛出IllegalStateException。
-            ViewParent vp = view.getParent();
-            if (vp != null) {
-                ViewGroup parent = (ViewGroup) vp;
-                parent.removeView(view);
-            }
-            container.addView(view);
-            //add listeners here if necessary
-            return view;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-
     }
 }
--- app/src/main/java/com/example/dell/doctor_platform/fragment/SetFragment.java
+++ app/src/main/java/com/example/dell/doctor_platform/fragment/SetFragment.java
@@ -3,18 +3,13 @@ package com.example.dell.doctor_platform.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.dell.doctor_platform.R;
 import com.example.dell.doctor_platform.gson.Person;
-
-import cn.bmob.v3.exception.BmobException;
-import cn.bmob.v3.listener.SaveListener;
 
 /*
  * Created by Kentan on 2017/7/4.
@@ -33,26 +28,6 @@ public class SetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
             container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_set, container, false);
-        add = (Button) view.findViewById(R.id.add_person);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                p1.setName("kentan");
-                p1.setAddress("America");
-                p1.save(new SaveListener<String>() {
-                    @Override
-                    public void done(String objectId, BmobException e) {
-                        if (e == null) {
-                            Log.d(LOG_TAG,"Succeeded");
-                            Toast.makeText(getContext(), "添加数据成功，返回objectId为：" + objectId, Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.d(LOG_TAG,"Fail");
-                            Toast.makeText(getContext(), "创建数据失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
         return view;
     }
 
